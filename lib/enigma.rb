07396e07f1ef3836684e580d0d154f4d8149edef
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-03 15:19:37 -0700

    Some deletes of irrelevant code

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -20,30 +20,7 @@ class Enigma
     @key_offsets << key_array[2..3].join.to_i
     @key_offsets << key_array[3..4].join.to_i
   end
-  #
-  # def offset_a
-  #   key_array = @key_generator.digits
-  #   a_array = []
-  #   a_array << key_array[0..1]
-  # end
-  # def offset_b
-  #   key_array = @key_generator.digits
-  #   b_array = []
-  #   b_array << key_array[1..2]
-  #   @offset_key_b = b_array.join.to_i
-  # end
-  # def offset_c
-  #   key_array = @key_generator.digits
-  #   c_array = []
-  #   c_array << key_array[2..3]
-  #   @offset_key_c = c_array.join.to_i
-  # end
-  # def offset_d
-  #   key_array = @key_generator.digits
-  #   d_array = []
-  #   d_array << key_array[1..2]
-  #   @offset_key_d = d_array.join.to_i
-  # end
+
 
 
 

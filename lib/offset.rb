ef423a984bc85bae059baaf29c874b98b469d0a7
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-06 13:57:29 -0700

    Argument revisions.

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,29 +1,33 @@
 require 'Date'
-require './lib/key'
+# require './lib/key'
 require 'pry'
 
 
 class DateOffset
 
-  def initialize(date = Date.today)
-    @date = date
-  end
+  # def initialize
+  #   @date = date
+  # end
 
-  def date_formatter
+  def date_formatter(date=Date.today)
     formatted_date_arr = []
-    date_to_string = @date.to_s
-    formatted_date_arr << date_to_string.split("-")[2]
-    formatted_date_arr << date_to_string.split("-")[1]
-    year = date_to_string.split("-")[0]
-    year = year.chars.join
-    year = year.slice(2,2)
-    formatted_date_arr << year
-    formatted_date_arr.join.to_i
+    date_to_string = date.to_s
+    if date_to_string.include?("-")
+      formatted_date_arr << date_to_string.split("-")[2]
+      formatted_date_arr << date_to_string.split("-")[1]
+      year = date_to_string.split("-")[0]
+      year = year.chars.join
+      year = year.slice(2,2)
+      formatted_date_arr << year
+      formatted_date = formatted_date_arr.join.to_i
+    else
+      formatted_date = date_to_string.to_i
+    end
   end
 
-  def date_squared
-    squared = (date_formatter ** 2).to_s.split("")
-    squared = squared.slice(6,4)
+  def date_squared(date=Date.today)
+    squared = (date_formatter(date) ** 2).to_s.split("")
+    squared = squared.slice(-4, 4)
     squared.map do |number|
       number.to_i
     end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,22 +1,39 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/offset'
+require 'pry'
 
 class OffsetTest < Minitest::Test
 
-  def test_date_offset_exists
+  def test_date_uses_current_date_as_arg
     date_offset = DateOffset.new
     date_offset.date_formatter
 
     assert_instance_of DateOffset, date_offset
   end
 
+  def test_takes_an_arguement
+    offset = DateOffset.new
+
+    offset.date_formatter(30415)
+
+    assert_instance_of DateOffset, offset
+  end
+
   def test_date_squared
-    date_offset = DateOffset.new
-    result = date_offset.date_squared.count
+    offset = DateOffset.new
+    result = offset.date_squared.count
 
     assert_equal 4, result
+  end
 
+  def test_date_arr_from_arv
+    offset = DateOffset.new
+
+    result = offset.date_squared("030415")
+    expected = [2, 2, 2, 5]
+
+    assert_equal expected, result
   end
 
 end

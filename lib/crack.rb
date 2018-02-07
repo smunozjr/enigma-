7d7bbede6e7498abc58a30fd8ed1d47f787e2f24
smunozjr1986 <sergiomunozjr07@gmail.com>
2018-02-07 13:05:23 -0700

    file structure

--- lib/crack.rb
+++ lib/crack.rb
@@ -10,5 +10,4 @@ output_file = File.open(ARGV[1], 'w')
 output_file.write(enigma.crack(input_text, ARGV[2]))
 output_file.close
 
-rotator = Rotator.new
 puts "Created 'cracked.txt' with the cracked key #{enigma.key} and date #{enigma.date}"
